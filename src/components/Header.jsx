e4dca2baa6dc8aa1ce3779696d4858237b2b2bd3
campagnamatias <campagnamatias@gmail.com>
2021-05-06 14:14:05 -0300

    poniendo links e iconos

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 import '../styles/components/Header.scss';
 
 const Header = ()=>{
     return(
         <div className="Header">
-            <h1 className="Header-title">Platziconf Merch</h1>
+            <h1 className="Header-title">
+                <Link to="/">Platziconf Merch</Link>
+            </h1>
             <div className="Header-checkout">
-                Checkout
+                <Link to="/checkout">
+                    <i className="fas fa-shopping-basket"></i>
+                </Link>
             </div>
         </div>
     )
--- src/containers/Payment.jsx
+++ src/containers/Payment.jsx
@@ -2,9 +2,17 @@ import React from 'react';
 import '../styles/components/Payment.scss';
 
 const Payment = () => {
-    return (
-        <h1>Payment</h1>
-    )
+  return (
+    <div className="Payment">
+      <div className="Payment-content">
+        <h3>Resument del pedido:</h3>
+        <div className="Payment-button">
+          Boton de pago con Paypal
+        </div>
+      </div>
+      <div />
+    </div>
+  );
 }
 
 export default Payment;
